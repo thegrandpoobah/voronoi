65d0aa6b2a1195b810cdd2adf0fbf0349e25aeeb
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2012-03-18 16:19:52 -0400

    Issue #19: Perform bounds checks where appropriate.

--- voronoi/parse_arguments.cpp
+++ voronoi/parse_arguments.cpp
@@ -56,6 +56,7 @@ std::auto_ptr<Voronoi::StipplingParameters> parseArguments( int argc, char *argv
 	using std::cout;
 	using std::cerr;
 	using std::endl;
+	using std::runtime_error;
 	using std::exception;
 
 	options_description requiredOpts( "Required Options" );
@@ -68,7 +69,7 @@ std::auto_ptr<Voronoi::StipplingParameters> parseArguments( int argc, char *argv
 
 	options_description basicOpts( "Basic Options" );
 	basicOpts.add_options()
-		( "stipples,s", value< unsigned int >()->default_value(4000), "Number of Stipple Points to use" )
+		( "stipples,s", value< int >()->default_value(4000), "Number of Stipple Points to use" )
 		( "colour-output,c", "Produce a coloured stipple drawing" );
 
 	options_description advancedOpts( "Advanced Options" );
@@ -77,7 +78,7 @@ std::auto_ptr<Voronoi::StipplingParameters> parseArguments( int argc, char *argv
 		( "no-overlap,n", "Ensure that stipple points do not overlap with each other" )
 		( "fixed-radius,f", "Fixed radius stipple points imply a significant loss of tonal properties" )
 		( "sizing-factor,z", value< float >()->default_value(1.0f, "1.0"), "The final stipple radius is multiplied by this factor" )
-		( "subpixels,p", value< unsigned int >()->default_value(5, "5"), "Controls the tile size of centroid computations." )
+		( "subpixels,p", value< int >()->default_value(5, "5"), "Controls the tile size of centroid computations." )
 		( "log,l", "Determines output verbosity" );
 
 	positional_options_description positional;
@@ -116,14 +117,26 @@ std::auto_ptr<Voronoi::StipplingParameters> parseArguments( int argc, char *argv
 		inputFile.copy(params->inputFile, inputFile.length());
 		params->outputFile = vm["output-file"].as<string>();
 
-		params->points = vm["stipples"].as<unsigned int>();
+		if (vm["stipples"].as<int>() <= 0) {
+			throw runtime_error("Stipple renderings must have at least 1 stipple point.");
+		}
+		params->points = (unsigned int)vm["stipples"].as<int>();
+		if (vm["threshold"].as<float>() < 0.005f) {
+			throw runtime_error("Convergence threshold parameter must be greater than 0.005");
+		}
 		params->threshold = vm["threshold"].as<float>();
 		params->createLogs = vm.count("log") > 0;
 		params->useColour = vm.count("colour-output") > 0;
 		params->noOverlap = vm.count("no-overlap") > 0;
 		params->fixedRadius = vm.count("fixed-radius") > 0;
+		if (vm["sizing-factor"].as<float>() < 0.0f) {
+			throw runtime_error("Sizing factor parameter must be greater than 0.");
+		}
 		params->sizingFactor = vm["sizing-factor"].as<float>();
-		params->subpixels = vm["subpixels"].as<unsigned int>();
+		if (vm["subpixels"].as<int>() <= 1) {
+			throw runtime_error("Sub-pixel density parameter must be greater than or equal to 1.");
+		}
+		params->subpixels = (unsigned int)vm["subpixels"].as<int>();
 
 		return params;
 	} catch ( exception const &e ) {
