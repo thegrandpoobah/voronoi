ddb5eb343491b71eb48cd3f1698cb08e46737d36
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2012-05-06 01:01:24 -0400

    Add convenience batch file.

---
+++ voronoi.bat
@@ -0,0 +1,5 @@
+@echo off
+rem This batch file allows easier execution from Windows Explorer.
+rem Modify the arguments below to specify the desired stippling options,
+rem and then rum the tool by double clicking the batch file.
+voronoi corpus\vase.png output.svg
