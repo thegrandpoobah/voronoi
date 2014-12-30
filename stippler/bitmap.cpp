c13c42cb204b946dccd2ddc44f34eb77813dbe43
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2014-12-30 00:30:55 -0500

    Fix a Visual Studio build warning.

--- stippler/bitmap.cpp
+++ stippler/bitmap.cpp
@@ -70,19 +70,19 @@ void Bitmap::getColour( float x, float y, unsigned char &r, unsigned char &g, un
 		f11 = fX * fY;
 
 	unsigned char *dataPtr = file->data + (((unsigned int)floor(y) * file->w + (unsigned int)floor(x)) * 4);
-	r = floor((float)(*(dataPtr)) * f00 + 
+	r = (unsigned char)floor((float)(*(dataPtr)) * f00 + 
 		(float)(*(dataPtr + 4)) * f10 +
 		(float)(*(dataPtr + file->w * 4)) * f01 +
 		(float)(*(dataPtr + file->w * 4 + 4)) * f11);
 
 	dataPtr++;
-	g = floor((float)(*(dataPtr)) * f00 + 
+	g = (unsigned char)floor((float)(*(dataPtr)) * f00 + 
 		(float)(*(dataPtr + 4)) * f10 +
 		(float)(*(dataPtr + file->w * 4)) * f01 +
 		(float)(*(dataPtr + file->w * 4 + 4)) * f11);
 
 	dataPtr++;
-	b = floor((float)(*(dataPtr)) * f00 + 
+	b = (unsigned char)floor((float)(*(dataPtr)) * f00 + 
 		(float)(*(dataPtr + 4)) * f10 +
 		(float)(*(dataPtr + file->w * 4)) * f01 +
 		(float)(*(dataPtr + file->w * 4 + 4)) * f11);
